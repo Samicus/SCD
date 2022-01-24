a970911b4c6b286b8160f0177ff969f97a23d02d
Samicus <samneme55@gmail.com>
2022-01-24 08:27:36 +0100

    .

--- DR-TANet-lightning/main.py
+++ DR-TANet-lightning/main.py
@@ -5,8 +5,9 @@ from params import encoder_arch, local_kernel_size, stride, padding, groups, drt
 
 
 #trainer = Trainer(gpus=1, fast_dev_run=True) # Debug
-trainer = Trainer(gpus=1, max_epochs=MAX_EPOCHS, default_root_dir=CHECKPOINT_DIR)
+
 for set_nr in range(2):
+    trainer = Trainer(gpus=1, max_epochs=MAX_EPOCHS, default_root_dir=pjoin(CHECKPOINT_DIR,"set{}".format(set_nr))
     data_module = PcdDataModule(set_nr)
     len_train_loader = len(data_module.train_dataloader())
     model = TANet(encoder_arch, local_kernel_size, stride, padding, groups, drtam, refinement, len_train_loader=len_train_loader)
