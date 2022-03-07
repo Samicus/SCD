ca9a3114569e392281271b483770c2a6bc5d9568
Arwin Saleh <arwinsaleh@hotmail.com>
2022-03-07 09:29:06 +0100

    yaml integrated into main

--- DR-TANet-lightning/data/augmentations.py
+++ DR-TANet-lightning/data/augmentations.py
@@ -1,14 +1,10 @@
-import random
+from random import random
 import numpy as np
 import cv2
 import math
 import os
 from os.path import join as pjoin, splitext as spt
 from PIL import Image
-<<<<<<< HEAD
-from params import scale, translate, rotation, mosaic_on, random_erase_on, albumentations_on, copy_paste_on, copy_paste_scale, copy_paste_rotation
-=======
->>>>>>> k_fold_cross_validation
 import albumentations as A
 
 
@@ -19,12 +15,6 @@ class DataAugment:
         self.mask_root = mask_root
         self.filename = filename
         self.index = None
-<<<<<<< HEAD
-
-        self.copy_paste = CopyPaste(t0_root, t1_root, mask_root, filename, scale=[copy_paste_scale[0],copy_paste_scale[1]], rotation=copy_paste_rotation)
-
-=======
->>>>>>> k_fold_cross_validation
         
         self.transform1 = A.Compose([
                 A.RandomShadow(p=.5),
@@ -34,20 +24,33 @@ class DataAugment:
         self.transform2 = A.Compose([
             A.HorizontalFlip(p=1),
             ])
+        
+        # Load Augmentation Parameters
         mosaic_params = aug_params["MOSAIC"]
         random_erase_params = aug_params["RANDOM_ERASE"]
         albumentation_params = aug_params["ALBUMENTATIONS"]
+        copy_paste_params = aug_params["COPY_PASTE"]
         
-        self.mosaic_aug = mosaic_params["mosaic_aug"]
+        # MOSAIC
+        self.mosaic_on = mosaic_params["mosaic_on"]
         self.mosaic_th = mosaic_params["mosaic_th"]
         self.translate = mosaic_params["translate"]
         self.scale = mosaic_params["scale"]
         self.rotation = mosaic_params["rotation"]
         
-        self.random_erase_aug = random_erase_params["random_erase_aug"]
+        # Random Erase
+        self.random_erase_on = random_erase_params["random_erase_on"]
         self.random_erase_th = random_erase_params["random_erase_th"]
         
-        self.albumentations_config = albumentation_params["albumentations_config"]
+        # Albumentations
+        self.albumentations_on = albumentation_params["albumentations_on"]
+        
+        # Copy Paste
+        copy_paste_scale = copy_paste_params["copy_paste_scale"]
+        copy_paste_rotation = copy_paste_params["copy_paste_rotation"]
+        self.copy_paste_on = copy_paste_params["copy_paste_on"]
+        self.copy_paste_th = copy_paste_params["copy_paste_th"]
+        self.copy_paste = CopyPaste(t0_root, t1_root, mask_root, filename, scale=[copy_paste_scale[0],copy_paste_scale[1]], rotation=copy_paste_rotation)
 
     def __call__(self, index):
         """
@@ -57,37 +60,25 @@ class DataAugment:
         self.img_t0_list = []
         self.img_t1_list = []
         self.img_mask_list = []
-<<<<<<< HEAD
-        if mosaic_on:
-=======
-        if self.mosaic_aug:
->>>>>>> k_fold_cross_validation
+        
+        mosaic_rand = random()
+        run_mosaic = (self.mosaic_on) and (mosaic_rand < self.mosaic_th)
+        
+        if run_mosaic:
             self.load_mosaic_imgs()
         else:
             self.load_current_img()
 
-        if copy_paste_on:
+        if self.copy_paste_on and random() < self.copy_paste_th:
             self.apply_copy_paste()
-        # albumentations_config = 0 -> no albumentation augment
-<<<<<<< HEAD
-        if albumentations_on:
+        if self.albumentations_on:
             # Augments data with albumentations, Which augments to be applied is chosen in params.py by albumentations_config
             self.albumentation_augment()
         # Apply random erase
-        if random_erase_on:
+        if self.random_erase_on and random() < self.random_erase_th:
             self.random_erase_augment(WIDTH_DIV=2.0, HEIGHT_DIV=2.0)
         #apply mosaic
-        if mosaic_on:
-=======
-        if not self.albumentations_config == 0:
-            # Augments data with albumentations, Which augments to be applied is chosen in params.py by albumentations_config
-            self.albumentation_augment()
-
-        if self.random_erase_aug:
-            self.random_erase_augment(WIDTH_DIV=2.0, HEIGHT_DIV=2.0)
-    
-        if self.mosaic_aug:
->>>>>>> k_fold_cross_validation
+        if run_mosaic:
             img_t0, img_t1, img_mask = self.mosaic_augment()
         else:
             img_t0, img_t1, img_mask = self.img_t0_list[0], self.img_t1_list[0], self.img_mask_list[0]
@@ -100,7 +91,6 @@ class DataAugment:
             self.img_t0_list[i], self.img_t1_list[i], self.img_mask_list[i] = self.copy_paste(self.img_t0_list[i], 
                                                                                               self.img_t1_list[i], 
                                                                                               self.img_mask_list[i])
-            
 
     def mosaic_augment(self, img_shape=(224,1024)):
         """
@@ -230,7 +220,7 @@ class DataAugment:
         # Rotation and Scale
         R = np.eye(3)
         a = random.uniform(-degrees, degrees)
-        if not degrees == 0 and random.random() >= .5:
+        if not degrees == 0 and random() >= .5:
             a += 180
         s = random.uniform(1 - scale, 1)
         R[:2] = cv2.getRotationMatrix2D(angle=a, center=(0, 0), scale=s)
@@ -294,7 +284,7 @@ class DataAugment:
                 transformed_t1 = self.transform1(image=t1_img)["image"]
 
                 # Horizontal flip on t0,t1,mask
-                if random.random() > .5:
+                if random() > .5:
                     transformed_t0 = self.transform2(image=transformed_t0)["image"]
                     transformed_t1 = self.transform2(image=transformed_t1)["image"]
                     transformed_mask = self.transform2(image=mask_img)["image"]
