72d63ce1aca9aa1814f73e69e42308f30312d687
Arwin Saleh <arwinsaleh@hotmail.com>
2022-03-09 15:12:03 +0100

    evaluating all sets

--- DR-TANet-lightning/eval.py
+++ DR-TANet-lightning/eval.py
@@ -1,27 +1,45 @@
 from network.TANet import TANet
 from pytorch_lightning import Trainer
 from data.DataModules import PCDdataModule
-from util import load_config
 import argparse
+import glob
+from os.path import join as pjoin
+from util import load_config
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-i", dest="ckpt", required=True)
-args = parser.parse_args()
-parser.add_argument("-c", "--config", required=True,
-	help="path to YAML")
+parser.add_argument("-i", "--checkpoints", required=True,
+	help="path to checkpoints")
+parser.add_argument("-e", "--experiment", required=True,
+	help="name of experiment")
+parser.add_argument("-c", "--configs", required=True,
+	help="path to configs")
 parser.add_argument("-d", "--dataset", required=True,
 	help="TSUNAMI or GSV?")
+parser.add_argument("-n", "--sets", required=True,
+	help="number of sets")
 parsed_args = parser.parse_args()
 
-config_path = parsed_args.config
-config = load_config(config_path)
-NUM_SETS = config["MISC"]["NUM_SETS"]
+NUM_SETS = int(parsed_args.sets)
+
+yaml_path = pjoin(parsed_args.configs, "**/{}.yaml".format(parsed_args.experiment))
+config_file = glob.glob(yaml_path)[0]
+
+config = load_config(config_file)
+AUG_PARAMS = config["AUGMENTATIONS"]
+AUGMENT_ON = config["MISC"]["AUGMENT_ON"]
+PCD_CONFIG = config["MISC"]["PCD_CONFIG"]
+PRE_PROCESS = config["MISC"]["PRE_PROCESS"]
+NUM_WORKERS = config["MISC"]["NUM_WORKERS"]
+BATCH_SIZE = config["MISC"]["BATCH_SIZE"]
+
 
 for set_nr in range(NUM_SETS):
+    current_experiment = "{}_PCD_set{}".format(parsed_args.experiment, set_nr)
+    checkpoint = glob.glob(pjoin(parsed_args.checkpoints, current_experiment, "**/**/epoch=*"))[0]
     model = TANet.load_from_checkpoint(
-                            checkpoint_path=args.ckpt,
+                            checkpoint_path=checkpoint,
                             map_location=None,
                             )
     trainer = Trainer(gpus=1)
-    data_module = PCDdataModule(set_nr, EVAL=parsed_args.dataset)
+    data_module = PCDdataModule(set_nr, AUG_PARAMS, AUGMENT_ON, PRE_PROCESS, PCD_CONFIG, NUM_WORKERS, BATCH_SIZE, EVAL=parsed_args.dataset)
     trainer.test(model, data_module)
