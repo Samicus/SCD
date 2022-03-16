a99f8a5541bd14f651d22d89d7935de6651a3c6f
Arwin Saleh <arwinsaleh@hotmail.com>
2022-03-16 09:16:17 +0000

    run scripts

---
+++ run_all_tests_0.sh
@@ -0,0 +1,8 @@
+CUDA_VISIBLE_DEVICES=0 python3 DR-TANet-lightning/main.py --config DR-TANet-lightning/config/augmentations/vanilla.yaml --det --aim --VL_CMU_CD
+CUDA_VISIBLE_DEVICES=0 python3 DR-TANet-lightning/main.py --config DR-TANet-lightning/config/augmentations/M.yaml --det --aim --VL_CMU_CD
+CUDA_VISIBLE_DEVICES=0 python3 DR-TANet-lightning/main.py --config DR-TANet-lightning/config/augmentations/CP.yaml --det --aim --VL_CMU_CD
+CUDA_VISIBLE_DEVICES=0 python3 DR-TANet-lightning/main.py --config DR-TANet-lightning/config/augmentations/RE.yaml --det --aim --VL_CMU_CD
+CUDA_VISIBLE_DEVICES=0 python3 DR-TANet-lightning/main.py --config DR-TANet-lightning/config/augmentations/A.yaml --det --aim --VL_CMU_CD
+CUDA_VISIBLE_DEVICES=0 python3 DR-TANet-lightning/main.py --config DR-TANet-lightning/config/augmentations/M_CP.yaml --det --aim --VL_CMU_CD
+CUDA_VISIBLE_DEVICES=0 python3 DR-TANet-lightning/main.py --config DR-TANet-lightning/config/augmentations/M_RE.yaml --det --aim --VL_CMU_CD
+CUDA_VISIBLE_DEVICES=0 python3 DR-TANet-lightning/main.py --config DR-TANet-lightning/config/augmentations/M_A.yaml --det --aim --VL_CMU_CD
---
+++ run_all_tests_1.sh
@@ -0,0 +1,8 @@
+CUDA_VISIBLE_DEVICES=1 python3 DR-TANet-lightning/main.py --config DR-TANet-lightning/config/augmentations/M_CP_RE.yaml --det --aim --VL_CMU_CD
+CUDA_VISIBLE_DEVICES=1 python3 DR-TANet-lightning/main.py --config DR-TANet-lightning/config/augmentations/M_CP_A.yaml --det --aim --VL_CMU_CD
+CUDA_VISIBLE_DEVICES=1 python3 DR-TANet-lightning/main.py --config DR-TANet-lightning/config/augmentations/M_RE_A.yaml --det --aim --VL_CMU_CD
+CUDA_VISIBLE_DEVICES=1 python3 DR-TANet-lightning/main.py --config DR-TANet-lightning/config/augmentations/M_CP_RE_A.yaml --det --aim --VL_CMU_CD
+CUDA_VISIBLE_DEVICES=1 python3 DR-TANet-lightning/main.py --config DR-TANet-lightning/config/augmentations/CP_RE.yaml --det --aim --VL_CMU_CD
+CUDA_VISIBLE_DEVICES=1 python3 DR-TANet-lightning/main.py --config DR-TANet-lightning/config/augmentations/CP_A.yaml --det --aim --VL_CMU_CD
+CUDA_VISIBLE_DEVICES=1 python3 DR-TANet-lightning/main.py --config DR-TANet-lightning/config/augmentations/CP_RE_A.yaml --det --aim --VL_CMU_CD
+CUDA_VISIBLE_DEVICES=1 python3 DR-TANet-lightning/main.py --config DR-TANet-lightning/config/augmentations/RE_A.yaml --det --aim --VL_CMU_CD
